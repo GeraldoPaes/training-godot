c425a467b9f8f83c7c1c0547121386d958d5e983
Geraldo Fernando <gfernandopaes@gmail.com>
2020-08-21 12:06:18 -0300

    Added Fall Animation and Capsule Collision to Player
    Now we can slide off edges and die :)

--- source/KinematicBody2D.gd
+++ source/KinematicBody2D.gd
@@ -6,7 +6,7 @@ const ACCELERATION = 50
 const MAX_SPEED = 200
 const JUMP_HEIGHT = -500
 
-# Friction values - Reduction rate percentage
+# Friction values - Speed Reduction Rate Percentage
 const AIR_FRICTION = 0.05
 const GROUND_FRICTION = 0.2
 
@@ -32,11 +32,15 @@ func _process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-		if friction: # Ground friction (Reduce h_speed % each frame)
+		if friction: # Ground friction (Reduces h_speed 20% each frame)
 			motion.x *= (1 - GROUND_FRICTION)
 	else:
-		$Sprite.play("Jump")
-		if friction: # Air friction (Reduce h_speed X% each frame)
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		
+		if friction: # Air friction (Reduces h_speed 5% each frame)
 			motion.x *= (1 - AIR_FRICTION)
 
 	motion = move_and_slide(motion, UP)
