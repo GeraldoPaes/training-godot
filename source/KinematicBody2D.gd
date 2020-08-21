fd030166cc6cdd14a3151befc734d57b5bc77425
Geraldo Fernando <gfernandopaes@gmail.com>
2020-08-20 21:18:50 -0300

    Initialization
    Putting the source project in the git

---
+++ source/KinematicBody2D.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const JUMP_HEIGHT = -500
+
+var motion = Vector2()
+
+func _process(delta):
+	
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else:
+		$Sprite.play("Idle")
+		motion.x = lerp(motion.x, 0, 0.2)
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("Jump")
+			
+	motion = move_and_slide(motion, UP)
+	
+	pass
